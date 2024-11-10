1cdc849bdf51c8b4d837503f65104b372cd59c44
wallacebraga <wallace@gmail.com>
2024-11-09 23:47:44 -0300

    Adicionado texto de apresentação

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-  return <h1>Sigam-me os bons! 🔨😁</h1>;
+  return <div>
+    <h1>Sigam-me os bons! 🔨😁</h1>
+    <p>Olá, eu sou o <strong>Wallace</strong> e estou aprendendo Next.js</p>
+  </div>;
 }
 
 export default Home;
