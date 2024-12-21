5b28261ad499bbdc81524f93e2d9f2cd8993d23b
Wallace <wallacedfb@gmail.com>
2024-12-21 08:46:53 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(req, res) {
+import database from '../../../../infra/database.js'
+
+async function status(req, res) {
+  const result = await database.query('SELECT NOW() AS NOW')
+  console.log(result.rows)
+
   res.status(200).json({
     status: 'ok',
     version: '1.0.0',
