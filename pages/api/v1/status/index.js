e5ef38bd43557aa5582b632f163217eb8ff3488a
Wallace <wallacedfb@gmail.com>
2024-12-26 17:33:18 -0300

    Finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,32 @@
 import database from 'infra/database.js'
 
 async function status(req, res) {
-  const result = await database.query('SELECT NOW() AS NOW')
-  console.log(result.rows)
+  const updatedAt = new Date().toISOString()
+
+  const dbVersionResult = await database.query('SHOW server_version;')
+  const dbVersionValue = dbVersionResult.rows[0].server_version
+
+  const dbMaxConnectionsResult = await database.query('SHOW max_connections;')
+  const dbMaxConnectionsValue = dbMaxConnectionsResult.rows[0].max_connections
+
+  const dbName = process.env.POSTGRES_DB
+
+  const dbOpenedConnectionsResult = await database.query({
+    text: 'SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;',
+    values: [dbName],
+  })
+
+  const dbOpenedConnectionsValue = dbOpenedConnectionsResult.rows[0].count
 
   res.status(200).json({
-    status: 'ok',
-    version: '1.0.0',
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnectionsValue),
+        opened_connections: dbOpenedConnectionsValue,
+      },
+    },
   })
 }
 
